3aee3a0a2cb588830c3e2cb28b73a23c818db9cb
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-11-05 16:20:02 +0300

    Removed annotations SendTo. ConvertAndSendToUser has been implemented instead of that

---
+++ src/main/resources/static/js/connection.js
@@ -0,0 +1,26 @@
+let callbacks = 0
+const stompClient = new StompJs.Client({
+    brokerURL: 'ws://localhost:8080/pig'
+});
+stompClient.onConnect = (frame) => {
+    console.log('Connected: ' + frame);
+    document.getElementById("start").disabled = false
+};
+
+function send(object, destination, disposable, callback ) {
+    if (stompClient.connected) {
+        stompClient.publish({
+            destination: "/app/" + destination,
+            body: JSON.stringify(object)
+        });
+        stompClient.subscribe("/players/game", (message) => {
+            callback(message.body)
+            callbacks++;
+            if (disposable) {
+                stompClient.unsubscribe(`sub-${callbacks - 1}`);
+            }
+        });
+    }
+}
+
+stompClient.activate();
